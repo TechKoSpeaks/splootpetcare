b01f415c9c94a7d558fc03c24dbe05495e0b872f
Travis Ko <tkospeaks@gmail.com>
2024-05-15 19:57:15 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+div.frame
+figure
+div.image-1
+- for (var i = 1; i <= 8; i++) div.image-2 div.text p Slice p Transition
+
+
+setTimeout(function(){
+    document.getElementById('demo').className = "hidden"; 
+ },10000);
